1921b47270f394f8afd6abeea2d231d6ceb288e0
eremi <ereminr83@mail.ru>
2023-07-28 18:24:10 +0700

    first commit

---
+++ src/main/java/com/example/kurs2/Kurs2Application.java
@@ -0,0 +1,13 @@
+package com.example.kurs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kurs2/Kurs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
